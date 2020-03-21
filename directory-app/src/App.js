63ceec6d878faa2d0f530a2e1c0d3b0a83d23101
Tyler <tgandle@gmail.com>
2020-03-20 23:06:30 -0500

    added table component, and fiddled with ALOT of stuff

--- directory-app/src/App.js
+++ directory-app/src/App.js
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Table from "./components/Table/Table";
-
-const headings = [
-  'Name',
-  'Age',
-  'Ethnicity',
-  'Phone Number',
-  'Emergency Contact',
-];
-
-const rows = [
-  [
-    "Tonia",
-    35,
-    "White",
-    867-5309,
-    "Matt",
-  ],
-];
-    
-
+import "./index.css"
 
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  console.log(searchTerm);
+
+  const headings = [
+    'Name',
+    'Age',
+    'Ethnicity',
+    'Phone Number',
+    'Emergency Contact',
+  ];
+
+  let rows = [
+    {name: "Tyler", age: 26, ethnicity: "White", phone: '867-5309', eContact: "Mom"},
+    {name: "Kay", age: 27, ethnicity: "White", phone: '356-5209', eContact: "Dad"},
+    {name: "Cassidy", age: 35, ethnicity: "Black", phone: '886-2237', eContact: "Grandpa"},
+    {name: "Stephen", age: 66, ethnicity: "Asian", phone: '568-5688', eContact: "Grandpa"},
+    {name: "James", age: 26, ethnicity: "White", phone: '999-2212', eContact: "Mom"},
+    {name: "Leslie", age: 98, ethnicity: "White", phone: '889-9043', eContact: "Mom"},
+    {name: "Tom", age: 33, ethnicity: "Black", phone: '777-7777', eContact: "Dad"},
+    {name: "Mary", age: 31, ethnicity: "Asian", phone: '442-2248', eContact: "Dad"},
+    {name: "Ellen", age: 21, ethnicity: "White", phone: '865-3343', eContact: "Aunt"},
+  ];
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const sortByName = (e) => {
+    console.log(e);
+    rows = rows.sort((a,b) => {
+      console.log(a);
+      console.log(b);
+      console.log('sorting');
+      if ( a.name < b.name ){
+          return -1;
+        }
+        if ( a.name > b.name ){
+          return 1;
+        }
+        return 0;
+    })
+  }
+
   return (
     <Router>
-      <div>
+      <div className="backgroundImg" >
         <Navbar/>
-        <Table headings={headings} rows={rows} />
-        {/*more stuff goes in here*/}
+        <input className='textbox' type='text' value={searchTerm} onChange={handleSearch}/>
+        <Table headings={headings} rows={rows} searchTerm={searchTerm} sortByName={sortByName}/>
       </div>
     </Router>
   );
--- directory-app/src/components/Table/Table.js
+++ directory-app/src/components/Table/Table.js
@@ -1,58 +1,38 @@
-import * as React from 'react';
-import Cell from './Cell';
+import React from 'react';
+import Cell from '../Cell/Cell.js';
 import './style.css';
 
-export default class Table extends React.Component {
-  renderHeadingRow = (_cell, cellIndex) => {
-    const {headings} = this.props;
-
-    return (
-      <Cell
-        key={`heading-${cellIndex}`}
-        content={headings[cellIndex]}
-        header={true}
-      />
-    )
-  };
-  
-  renderRow = (_row, rowIndex) => {
-    const {rows} = this.props;
-
-    return (
-      <tr key={`row-${rowIndex}`}>
-        {rows[rowIndex].map((_cell, cellIndex) => {
-          return (
-            <Cell
-              key={`${rowIndex}-${cellIndex}`}
-              content={rows[rowIndex][cellIndex]}
-            />
-          )
-        })}
-      </tr>
-    )
-  };
-
-  render() {
-    const {headings, rows} = this.props;
-
-    this.renderHeadingRow = this.renderHeadingRow.bind(this);
-    this.renderRow = this.renderRow.bind(this);
-    
-    const theadMarkup = (
-      <tr key="heading">
-        {headings.map(this.renderHeadingRow)}
-      </tr>
-    );
-
-    const tbodyMarkup = rows.map(this.renderRow);
-  
-    return (
-      <table className="Table">
-        <thead>{theadMarkup}</thead>
-        <tbody>{tbodyMarkup}</tbody>
-      </table>
-    );
+const Table = props => {
+  const { headings, rows, searchTerm, sortByName } = props
+  console.log(sortByName);
+  const theadMarkup = () => {
+    return headings.map(heading=>{
+      return <th>{heading}</th>
+    })
   }
+
+  const tbodyMarkup = () => {
+
+    return rows.filter(row=> row.name.includes(searchTerm) || row.ethnicity.includes(searchTerm) || row.eContact.includes(searchTerm)).map(row => {
+      return (
+        <tr>
+          <td onClick={sortByName}>{row.name}</td>
+          <td>{row.age}</td>
+          <td>{row.ethnicity}</td>
+          <td>{row.phone}</td>
+          <td>{row.eContact}</td>
+        </tr>
+      )
+    })
+  }
+
+  return (
+    <table className="Table">
+      <thead><tr>{theadMarkup()}</tr></thead>
+      <tbody>{tbodyMarkup()}</tbody>
+      <img src="" />
+    </table>
+  )
 }
 
-
+export default Table
